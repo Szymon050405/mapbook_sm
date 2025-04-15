cd3a8887ae48a0f91f3241b677db1c09642f1409
Szymon050405 <molenda.szymon05@gmail.com>
2025-04-15 19:42:16 +0200

    Feat: Add read functionality

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utilis.model import users
 from utilis.controller import get_user_info
 
 def main()
-    print(f"Witaj {users[0]["name"]}")
+print(f"Witaj {users[0]["name"]}")
     get_user_info(users[1:])
 
 if __name__ == "__main__":
