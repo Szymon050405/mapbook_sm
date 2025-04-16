f4a67ff2139aa7737e5ea9a89ea14100783e8497
Szymon050405 <molenda.szymon05@gmail.com>
2025-04-16 10:56:19 +0200

    wip: new function add new user

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from utilis.model import users
-from utilis.controller import get_user_info
+from utilis.controller import get_user_info, add_user
+
 
 def main():
     while True:
         print("=================MENU=================")
         print("0 - zakończ program")
         print("1 - Pokaż co u znajomych")
+        print("2 - Dodaj nowego znajomego")
         print("======================================")
         choice = input("wybierz opcje menu: ")
         if choice == "0": break
         if choice == "1": get_user_info(users)
+        if choice == "2": add_user(users)
+
 
 
 if users == "main":
     main()
-
--- utilis/controller.py
+++ utilis/controller.py
@@ -2,3 +2,9 @@ def get_user_info(users_data:list)->None:
     for user in users_data:
         print(f"Twój znajomy {user['name']}! z miejscowości {user['location']} opublikował {user['posts']} postów")
 
+
+def add_user(users_data: list) -> None:
+    user_name = input("Podaj imię nowego użytkownika: ")
+    user_location = input("Podaj lokalizacje nowego znajomego: ")
+    user_posts = int(input("Podaj liczbę postów: "))
+    users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
