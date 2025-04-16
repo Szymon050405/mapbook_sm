68e1ac2e10f9926ce8b5991638dfb6b6f6a2a384
Szymon050405 <molenda.szymon05@gmail.com>
2025-04-16 11:13:12 +0200

    wip: new function remove user

--- notatnik.py
+++ notatnik.py
@@ -1,6 +1,17 @@
-while True:
-        day:str=input("Jaki dziś dzień")
-        if day == "środa":
-                print("Brawo zgadłeś")
-                break
-        print("Nie zgadłeś spróbuj jeszcze raz")
+users: list = [
+    {"name":"Szymon","location":"Pruszków","posts":400},
+
+
+]
+print(users)
+
+def remove_user(users_data: list)->None:
+    user_name=input("Podaj imie użytkownika do usunięcia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
+
+remove_user(users)
+
+
+print(users)
--- utilis/controller.py
+++ utilis/controller.py
@@ -1,4 +1,4 @@
-def get_user_info(users_data:list)->None:
+def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f"Twój znajomy {user['name']}! z miejscowości {user['location']} opublikował {user['posts']} postów")
 
@@ -8,3 +8,10 @@ def add_user(users_data: list) -> None:
     user_location = input("Podaj lokalizacje nowego znajomego: ")
     user_posts = int(input("Podaj liczbę postów: "))
     users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
+
+
+def remove_user(users_data: list) -> None:
+    user_name = input("Podaj imie użytkownika do usunięcia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
