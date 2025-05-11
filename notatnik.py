5300967f1a9aeabc4302b9af10f5889ae5152126
Szymon050405 <molenda.szymon05@gmail.com>
2025-05-11 21:15:51 +0200

    wip: new function remove user

--- notatnik.py
+++ notatnik.py
@@ -1,5 +1,19 @@
-def  tomek(name:str)->None:
-        print(f"Cześć {name}, jestem Tomek")
+users:list = [
+    {"name":"Zuzia","location":"Radzyń_Podlaski","posts":700},
 
-user_name="Szymon"
-tomek(user_name)
+]
+print(users)
+
+def remove_user(users_data: list)->None:
+
+    user_name=input("podaj imię użytkownika do usuniecia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
+
+remove_user(users)
+
+
+
+
+print(users)
--- utilis/model.py
+++
@@ -1,6 +0,0 @@
-users: list = [
-    {"name":"Szymon","location":"Pruszków","posts":400},
-    {"name":"Krzysztof","location":"Białobrzegi","posts":500},
-    {"name":"Maja","location":"Świecie","posts":300},
-    {"name":"Zuzanna","location":"Radzyń Podlaski","posts":700},
-]
