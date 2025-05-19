e351ccd19ac95f6511511eafc66e66979cf136d9
Szymon050405 <molenda.szymon05@gmail.com>
2025-05-19 06:59:59 +0200

    wip: new function update user

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from utilss.model import users
-from utilss.controller import get_user_info, add_user, remove_user, update_user, get_map
+from tkinter import *
 
 
-def main():
-    while True:
 
-        print('=============MENU===============')
-        print('0 - zakończ program')
-        print('1 - poka co u znajomych')
-        print('2 - dodaj nowego znajmego')
-        print('3 - usuń znajomego')
-        print('4 - aktualizuj dane znajomego')
-        print('5 - Wygeneruj mape znajomych')
-        print('===================================')
-        choice = input('wybierz opcję menu: ')
-        if choice == '0': break
-        if choice == '1': get_user_info(users)
-        if choice == '2': add_user(users)
-        if choice == '3': remove_user(users)
-        if choice == '4': update_user(users)
-        if choice == '5': get_map(users)
+root = Tk()
+root.geometry("1200x760")
+root.title("mapbook")
 
 
-if __name__ == "__main__":
-    main()
+
+
+
+root.mainloop()
